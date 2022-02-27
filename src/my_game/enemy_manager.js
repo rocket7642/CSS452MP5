494468b06172c5c6053bf389a074280bda845400
alexhilwa <alexanderhilwa@gmail.com>
2022-02-26 17:05:14 -0800

    added special enemy
    has 3 wings, spawns 1/5 chance instead of a normal enemy

--- src/my_game/enemy_manager.js
+++ src/my_game/enemy_manager.js
@@ -2,6 +2,7 @@
 
 import engine from "../engine/index.js";
 import EnemyGroup from "./objects/enemy_group.js";
+import EnemyGroupSpecial from "./objects/enemy_group_special.js"
 
 class EnemyManager
 {
@@ -16,7 +17,19 @@ class EnemyManager
     {
         let xSpawn = (Math.random() * (200 - 101));
         let ySpawn = (Math.random() * (112 - 38) - 38);
-        let newEnemy = new EnemyGroup(this.spriteTexture, xSpawn, ySpawn);
+
+        let newEnemy = null;
+        //let r = Math.random * 5;
+        //console.log(r);
+        if((Math.random() * 5) > 4)
+        {
+            console.log("Spawning special enemy");
+            newEnemy = new EnemyGroupSpecial(this.spriteTexture, xSpawn, ySpawn);
+        }
+        else
+        {
+            newEnemy = new EnemyGroup(this.spriteTexture, xSpawn, ySpawn);
+        }
         this.enemySet.push(newEnemy);
         //console.log("enemy at: " + xSpawn + " " + ySpawn);
     }
